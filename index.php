1e2e85a3c8da50ffa30d3817ba54f2cdbace5073
Carlo Mendoza <carlomendoza89@gmail.com>
2019-01-08 21:25:21 -0800

    added myself to students

--- index.php
+++ index.php
@@ -31,6 +31,20 @@
         $second->add_grade(50);
         $students['a456'] = $second;
 
+        $me = new Student();
+        $me->surname = "Mendoza";
+        $me->first_name = "Carlo";
+        $me->add_email('home','carlomendoza89@gmail.com');
+        $me->add_email('school','cmendoza10@my.bcit.ca');
+        $me->add_grade(87);
+        $me->add_grade(86);
+        $me->add_grade(98);
+        $me->add_grade(97);
+        $me->add_grade(98);
+        $students['a00967251'] = $me;
+
+        ksort($students); // one of the many sort functions
+
         foreach($students as $student)
             echo $student->toString();
         ?>
